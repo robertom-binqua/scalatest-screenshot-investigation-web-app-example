bd6c59df0de73fe41cb0ac447f1cf3a38b05af7c
roberto malagigi <4958044+robertom-binqua@users.noreply.github.com>
2025-04-29 16:19:17 +0100

    using given to replace implicit

--- src/main/scala/org/binqua/scalatest/reporter/TestOutcome.scala
+++ src/main/scala/org/binqua/scalatest/reporter/TestOutcome.scala
@@ -3,13 +3,13 @@ package org.binqua.scalatest.reporter
 import io.circe.{Encoder, Json}
 
 sealed trait TestOutcome
-object TestOutcome {
 
-  implicit val encoder: Encoder[TestOutcome] = (a: TestOutcome) => Json.fromString(a.toString.toLowerCase)
+object TestOutcome:
+
+  given encoder: Encoder[TestOutcome] = (testOutcome: TestOutcome) => Json.fromString(testOutcome.toString.toLowerCase)
 
   case object SUCCEEDED extends TestOutcome
 
   case object FAILED extends TestOutcome
 
   case object STARTING extends TestOutcome
-}
